9579e0c8e91679d67182120484bf70128e2daa39
kimwjdtn <rhfemewjdtn@gmail.com>
2025-09-12 22:46:25 +0900

    init

---
+++ Gusto-iOSTests/Gusto_iOSTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import Gusto_iOS
+
+struct Gusto_iOSTests {
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
