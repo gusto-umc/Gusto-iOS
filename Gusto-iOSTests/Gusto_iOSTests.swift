b01e3bf01820cc921249ef48498b7b8d67e2393b
kimwjdtn <rhfemewjdtn@gmail.com>
2025-09-22 21:40:09 +0900

    log, network 클래스 추가

--- Gusto-iOSTests/Gusto_iOSTests.swift
+++ Gusto-iOSTests/Gusto_iOSTests.swift
@@ -1,10 +1,40 @@
 import Testing
+import Foundation
 @testable import Gusto_iOS
 
 struct Gusto_iOSTests {
   
   @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    struct TestRequest: Requestable {
+      var path: String
+      var method: HTTPMethod
+      var headers: [String : String]?
+      var parameters: [String : Any]?
+      var body: Data?
+      var retry: Int
+      var delay: Duration
+    }
+    
+    func checkDuplicateNickName(name: String) async throws {
+      do {
+        try await NetworkProvider().request(
+          TestRequest(
+            path: "/users/check-nickname/\(name)",
+            method: .get,
+            retry: 1,
+            delay: .seconds(0.5)
+          )
+        )
+      } catch {
+        throw NetworkError.responseError(statusCode: 409)
+      }
+    }
+    do {
+      try await checkDuplicateNickName(name: "TEST")
+      print("success")
+    } catch {
+      print("error: \(error)")
+    }
   }
   
 }
