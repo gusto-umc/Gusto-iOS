425ed855d4d97a4b09106d4d73f1980e96bbb47d
kangddong <rkdehddud96@naver.com>
2025-10-16 18:42:19 +0900

    [ADD] SearchTextField 파일 추가

--- DesignSystem/Sources/GustoComponent/GustoComponent.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GustoComponent.swift
-//  DesignSystem
-//
-//  Created by 강동영 on 10/16/25.
-//
-
-// 패키지 인식용 Stub 파일
---
+++ DesignSystem/Sources/GustoComponent/SearchTextField.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTextField.swift
+//  DesignSystem
+//
+//  Created by 강동영 on 10/16/25.
+//
+
+import SwiftUI
+
+struct SearchTextField: View {
+    var body: some View {
+      Text("Hello, World!")
+    }
+}
+
+#Preview {
+  SearchTextField()
+}
