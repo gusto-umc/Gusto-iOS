8dc73e687a0b7a7fa557f4b9cea76e94740d28de
kimwjdtn <rhfemewjdtn@gmail.com>
2025-10-26 20:17:26 +0900

    [Core] 탭바를 모듈화에 맞게 수정

--- Gusto-iOS/Feature/AppFeature/AppFeature.swift
+++ Gusto-iOS/Feature/AppFeature/AppFeature.swift
@@ -1,6 +1,6 @@
 import ComposableArchitecture
 import SwiftUI
-import Foundation
+//import Tab1 //탭모듈
 
 public enum Tab: String, Equatable, CaseIterable {
   case map
@@ -10,7 +10,14 @@ public enum Tab: String, Equatable, CaseIterable {
   case my
 }
 
-public enum Path {}
+@Reducer
+public enum AppPath {
+  //각 탭의 Path를 연관값으로 들고 있어 접근 가능
+  case tab1(Tab1Paths)
+  //case feed(FeedPaths)
+  //...
+}
+
 @Reducer
 struct AppFeature {
   @ObservableState
@@ -42,44 +49,6 @@ struct AppFeature {
   }
 }
 
-@Reducer
-public struct TabBarFeature {
-  @ObservableState
-  public struct State {
-    var selectedTab: Tab = .map
-    var tab1: TMPFeature.State = .init(txt: "1")
-    var tab2: TMPFeature.State = .init(txt: "2")
-  }
-  public enum Action {
-    case selectedTab(Tab)
-    case tab1(TMPFeature.Action)
-    case tab2(TMPFeature.Action)
-  }
-  public var body: some ReducerOf<Self> {
-    Scope(state: \.tab1, action: \.tab1) {
-      TMPFeature()
-    }
-    Scope(state: \.tab2, action: \.tab2) {
-      TMPFeature()
-    }
-    Reduce { state, action in
-      switch action {
-      case .selectedTab(let tab):
-        state.selectedTab = tab
-        return .none
-      case .tab1(.delegate(.routing(let link))):
-        //탭 간 깊은 화면 전환
-        //link == "/my/setting/changeName(user: 1e341)"
-        return .none
-      case .tab2(.delegate):
-        return .none
-      default:
-        return .none
-      }
-    }
-  }
-}
-
 struct AppView: View {
   let store: StoreOf<AppFeature>
   
@@ -97,29 +66,82 @@ struct AppView: View {
   }
 }
 
+@Reducer
+public struct TabBarFeature {
+  //스택을 탭이 아닌 TabBar에서 소유
+  //!!!: 모든 화면의 모든 store의 action을 이곳에서 처리해야합니다.
+  //TODO: 각 탭으로 분리할 수 있으면 좋겠네요
+  //각 탭으로 분리를 한다면 이를 위해 Stack을 @Shared로 각 탭Feature에 공유 가능
+  //note: 각 탭 별 스택을 사용하지만, 단일 StackState를 사용할 수도 있습니다.
+  @ObservableState
+  public struct State {
+    var selectedTab: Tab = .map
+    var tab1Stack: StackState<AppPath.State> = .init()
+  }
+  public enum Action {
+    case selectedTab(Tab)
+    case tab1Stack(StackActionOf<AppPath>)
+    
+    case tmppush
+  }
+  public var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .selectedTab(let tab):
+        state.selectedTab = tab
+        return .none
+        
+      case .tmppush:
+        state.tab1Stack.append(.tab1(.탭1의화면1(TMPFeature.State(txt: "테스트"))))
+        return .none
+        
+      case .tab1Stack(.element(id: _, action: .tab1(.탭1의화면1(.delegate(.back))))):
+        print("backButtonTapped")
+        return .none
+        
+      default:
+        return .none
+      }
+    }
+  }
+}
 struct TabBarView: View {
   @Bindable var store: StoreOf<TabBarFeature>
   
   var body: some View {
     ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
       TabView(selection: $store.selectedTab.sending(\.selectedTab)) {
-        Text("지도")
-          .tag(Tab.map)
-          .toolbar(.hidden, for: .tabBar)
+        NavigationStack(path: $store.scope(state: \.tab1Stack, action: \.tab1Stack)) {
+          Button {
+            store.send(.tmppush)
+          } label: {
+            Text("asdf")
+          }
+        } destination: { pathStore in
+          //코드가 너무 길어져서 뷰 생성을 다른 곳으로 이동
+          CentralDestinationRouter(pathStore: pathStore)
+        }
+        .tag(Tab.map)
+        .toolbar(.hidden, for: .tabBar)
         Text("리뷰")
+        //...
           .tag(Tab.review)
           .toolbar(.hidden, for: .tabBar)
         Text("리스트")
+        //...
           .tag(Tab.list)
           .toolbar(.hidden, for: .tabBar)
         Text("먹스또")
+        //...
           .tag(Tab.feed)
           .toolbar(.hidden, for: .tabBar)
         Text("마이")
+        //...
           .tag(Tab.my)
           .toolbar(.hidden, for: .tabBar)
       }
-
+      
+      
       HStack {
         ForEach(Tab.allCases, id: \.self) { tab in
           Spacer()
@@ -131,20 +153,86 @@ struct TabBarView: View {
         }
         Spacer()
       }
-      .padding(.bottom, 10)
+      .padding(.vertical, 10)
+      .background {
+        Color.black
+      }
     }
     .ignoresSafeArea(edges: [.bottom, .horizontal])
   }
 }
+
+struct CentralDestinationRouter<PathState, PathAction>: View {
+  let pathStore: Store<PathState, PathAction>
+  var body: some View {
+    if let pathStore = pathStore as? StoreOf<AppPath> {
+      switch pathStore.case {
+      case .tab1(let tab1Store):
+        if let store = tab1Store.scope(state: \.탭1의화면1, action: \.탭1의화면1) {
+          TMPView(store: store)
+        } else if let store = tab1Store.scope(state: \.탭1의화면2, action: \.탭1의화면2) {
+          TMPView2(store: store)
+        } else {
+          EmptyView()
+        }
+      //case .feed(let feedStore):
+        //...
+      @unknown default:
+        EmptyView()
+      }
+    } else {
+      EmptyView()
+    }
+  }
+}
+
+
+
+
+
+
+
 #Preview {
-  AppView(store: Store(initialState: AppFeature.State.onboarding(TMPonboardingFeature.State()), reducer: {
-    AppFeature()
+  TabBarView(store: Store(initialState: TabBarFeature.State(), reducer: {
+    TabBarFeature()
   }))
 }
 
 
 
-//MARK: - 빌드하기 위한 임시 코드
+
+
+
+//MARK: - 탭 모듈
+
+@Reducer
+public struct Tab1Paths {
+  public init() {}
+  @ObservableState
+  public enum State {
+    case 탭1의화면1(TMPFeature.State)
+    case 탭1의화면2(TMPFeature2.State)
+  }
+  public enum Action {
+    case 탭1의화면1(TMPFeature.Action)
+    case 탭1의화면2(TMPFeature2.Action)
+  }
+  public var body: some ReducerOf<Self> {
+    Scope(state: \.탭1의화면1, action: \.탭1의화면1) {
+      TMPFeature()
+    }
+    Scope(state: \.탭1의화면2, action: \.탭1의화면2, child: {
+      TMPFeature2()
+    })
+  }
+}
+//!!!: 모듈화로 접근 제한이 생겨서 다음과 같이 public 선언해도 이니셜라이저 못 찾아서 사용 불가능합니다. 위처럼 명시적으로 생성해야합니다.
+//public enum Tab1Paths {
+//  case 탭1의화면1(TMPFeature)
+//  case 탭1의화면2(TMPFeature)
+//}
+
+
 @Reducer
 struct TMPonboardingFeature {
   struct State {}
@@ -174,20 +262,16 @@ struct TMPonboardingView: View {
 
 @Reducer
 public struct TMPFeature {
+  public init(){}
   @ObservableState
   public struct State {
-    var txt: String
+    public var txt: String
+    public init(txt: String) { self.txt = txt }
   }
   public enum Action {
     case delegate(Delegate)
-    //NOTE: delegate 내부 라우팅
     public enum Delegate {
       case back
-      case routing(Any)
-    }
-    //NOTE: 별도 라우팅
-    case routing(Routing)
-    public enum Routing {
       case routing(Any)
     }
   }
@@ -211,146 +295,36 @@ public struct TMPFeature {
   }
 }
 public struct TMPView: View {
-  let store: StoreOf<TMPFeature>
-
+  @Bindable var store: StoreOf<TMPFeature>
+  public init(store: StoreOf<TMPFeature>) {
+    self.store = store
+  }
+  public var body: some View {
+    Text(store.txt)
+    Button("back") {
+      store.send(.delegate(.back))
+    }
+  }
+}
+@Reducer
+public struct TMPFeature2 {
+  public init(){}
+  @ObservableState
+  public struct State {
+    public var txt: String
+    public init(txt: String) { self.txt = txt }
+  }
+  public enum Action {
+  }
+  public var body: some ReducerOf<Self> {
+  }
+}
+public struct TMPView2: View {
+  @Bindable var store: StoreOf<TMPFeature2>
+  public init(store: StoreOf<TMPFeature2>) {
+    self.store = store
+  }
   public var body: some View {
     Text(store.txt)
   }
-}
-
-//MARK: - 탭 별 Path 관리
-@Reducer
-public struct 탭1루트피쳐 {
-  @ObservableState
-  public struct State {
-    var path = StackState<Paths.State>()
-  }
-  public enum Action {
-    case path(StackActionOf<Paths>)
-    case append(Paths.State)
-    case delegate(Delegate)
-    
-    case onAppear
-    
-    public enum Delegate {
-      case deepLink(Any)
-      case logout
-      case login
-    }
-  }
-  public var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .append(let nextPathState):
-        state.path.append(nextPathState)
-        return .none
-      default:
-        return .none
-      }
-    }
-    .forEach(\.path, action: \.path)
-  }
-  
-  @Reducer
-  public enum Paths {
-    case 탭1의화면1(TMPFeature)
-    case 탭1의화면2(TMPFeature)
-  }
-}
-
-
-//MARK: - 탭 별 destination 관리
-public struct 탭1루트뷰: View {
-  @Bindable var store: StoreOf<탭1루트피쳐>
-  private struct Constant {
-    var leftInRectanglePadding: CGFloat = 32
-    var Fonasdt: Font = .body
-  }
-  
-  public var body: some View {
-    NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-      Button("1") {
-        store.send(.append(.탭1의화면1(TMPFeature.State(txt: "TEST"))))
-      }
-    } destination: { store in
-      switch store.case {
-      case .탭1의화면1(let store):
-        TMPView(store: store)
-      default:
-        EmptyView()
-      }
-    }
-  }
-}
-
-#Preview("예시") {
-  탭1루트뷰(store: Store(initialState: 탭1루트피쳐.State(), reducer: {
-    탭1루트피쳐()
-  }))
-}
- // !!!: destination관리하는 Path를 전역으로 둔다면 이렇게
-//전역관리
-extension Path {
-  @Reducer
-  enum Onboarding {
-    case onboarding(TMPonboardingFeature)
-  }
-}
-extension Path {
-  @Reducer
-  public enum Tab1 {
-    case one(TMPFeature)
-    case two(TMPFeature)
-    //...
-  }
-}
-
-// MARK: - ViewDestination의 중앙 처리
-
-struct CentralDestinationRouter<PathState, PathAction>: View {
-  let pathStore: Store<PathState, PathAction>
-  @Dependency(\.log) var logger
-  var body: some View {
-    if let tab1Store = pathStore as? StoreOf<탭1루트피쳐.Paths> {
-      switch tab1Store.case {
-      case .탭1의화면1(let nextStore):
-        TMPView(store: nextStore)
-      default:
-        let _ = logger.log("tab1Store default", category: .warning)
-      }
-    } else if let tab1Store = pathStore as? StoreOf<Path.Tab1> {
-      switch tab1Store.case {
-      case .one(let nextFeature):
-        TMPView(store: nextFeature)
-      default:
-        let _ = logger.log("tab1Store default", category: .warning)
-        EmptyView()
-      }
-    } else {
-      let _ = logger.log("타입 변환 실패", category: .warning)
-      EmptyView()
-    }
-  }
-}
-
-struct Tab1CenteralizeView: View {
-  @Bindable var store: StoreOf<탭1루트피쳐>
-  
-  var body: some View {
-    NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-      Button {
-        store.send(.append(.탭1의화면1(TMPFeature.State(txt: "Central"))))
-      } label: {
-        Text("append")
-      }
-    } destination: { nextStore in
-      CentralDestinationRouter(pathStore: nextStore)
-    }
-  }
-}
-
-#Preview("central") {
-  Tab1CenteralizeView(store: Store(initialState: 탭1루트피쳐.State(), reducer: {
-    탭1루트피쳐()
-  }))
 }
