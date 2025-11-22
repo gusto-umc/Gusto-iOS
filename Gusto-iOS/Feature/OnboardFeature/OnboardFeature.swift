1c785447083edf44dd58a94cd420db884a31c6ee
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-22 19:59:46 +0900

    [chore] UI 구현 참고를 위한 SampleView들 추가

--- Gusto-iOS/Feature/OnboardFeature/OnboardFeature.swift
+++ Gusto-iOS/Feature/OnboardFeature/OnboardFeature.swift
@@ -9,17 +9,26 @@ import ComposableArchitecture
 
 @Reducer
 struct OnboardFeature {
+    // MARK: state
     @ObservableState
     struct State {
+        var path = StackState<Path.State>()
+    }
+    
+    @Reducer
+    enum Path {
         
     }
     
     
+    // MARK: saction
     enum Action {
-        
+        case path(StackActionOf<Path>)
     }
     
     var body: some Reducer<State, Action> {
-        
+        Reduce { state, action in
+            return .none
+        }
     }
 }
