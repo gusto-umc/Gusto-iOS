27c16d218f2c4ddc5383df2cf5d4df92c6ee7152
kimwjdtn <48548017+kimwjdtn@users.noreply.github.com>
2025-11-27 15:29:49 +0900

    [feat] TabBar의 UI를 구현했습니다 (#21)
    * [feat] 탭바의 UI를 구현했습니다

--- Gusto-iOS/Feature/TabBarFeature/TabBarFeatureView.swift
+++ Gusto-iOS/Feature/TabBarFeature/TabBarFeatureView.swift
@@ -5,48 +5,34 @@ struct TabBarFeatureView: View {
   @Bindable var store: StoreOf<TabBarFeature>
   
   var body: some View {
-    ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-      TabView(selection: $store.selectedTab.sending(\.selectedTab)) {
+    ZStack(alignment: .bottom) {
+      TabView(selection: $store.selectedTab) {
         Text("지도")
           .tag(Tab.map)
-          .toolbar(.hidden, for: .tabBar)
+          .toolbarVisibility(.hidden, for: .tabBar)
         Text("리뷰")
           .tag(Tab.review)
-          .toolbar(.hidden, for: .tabBar)
+          .toolbarVisibility(.hidden, for: .tabBar)
         Text("리스트")
           .tag(Tab.list)
-          .toolbar(.hidden, for: .tabBar)
+          .toolbarVisibility(.hidden, for: .tabBar)
         Text("먹스또")
           .tag(Tab.feed)
-          .toolbar(.hidden, for: .tabBar)
+          .toolbarVisibility(.hidden, for: .tabBar)
         Text("마이")
           .tag(Tab.my)
-          .toolbar(.hidden, for: .tabBar)
+          .toolbarVisibility(.hidden, for: .tabBar)
       }
       
-      HStack {
-        ForEach(Tab.allCases, id: \.self) { tab in
-          Spacer()
-          Button {
-            store.send(.selectedTab(tab))
-          } label: {
-            Text(tab.rawValue)
-          }
-        }
-        Spacer()
-      }
-      .padding(.vertical, 10)
-      .background {
-        Color.black
-      }
+      TabBarView(store: store)
     }
     .ignoresSafeArea(edges: [.bottom, .horizontal])
   }
 }
-
 
 #Preview {
   TabBarFeatureView(store: Store(initialState: TabBarFeature.State(), reducer: {
     TabBarFeature()
   }))
 }
+
