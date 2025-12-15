c04eecb7e4d0c2fce2128b71dbd30d762cc1c979
mandooplz <kmw4561@gmail.com>
2025-12-15 16:48:04 +0900

    [feat] SetNicknameFeature.validateInput() 구현

--- Gusto-iOS/Feature/OnboardFeature/SetNicknameFeature.swift
+++ Gusto-iOS/Feature/OnboardFeature/SetNicknameFeature.swift
@@ -5,11 +5,16 @@
 //  Created by 김민우 on 12/14/25.
 //
 import ComposableArchitecture
+import OSLog
 
 
 // MARK: Feature
 @Reducer
 struct SetNicknameFeature {
+    // MARK: core
+    private let logger = Logger()
+    
+    
     // MARK: state
     @ObservableState
     struct State {
@@ -28,9 +33,17 @@ struct SetNicknameFeature {
             switch action {
             case .validateInput:
                 // capture
+                let nickname = state.nicknameInput
                 
+                // process
+                let isNicknameEmpty = nickname.isEmpty
+                let isNicknameTaken = (nickname == "김철수")
+                
+                let isValid = !isNicknameEmpty && !isNicknameTaken
                 
                 // mutate
+                state.isNicknameValid = isValid
+                return .none
             }
         }
     }
